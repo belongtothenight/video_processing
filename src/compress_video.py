e43461628228a98c3aa8e33606d54bf2d49fd573
belongtothenight <dachuan516@gmail.com>
2022-11-19 18:56:34 +0800

    compress_video.py v1.0.9

--- src/compress_video.py
+++ src/compress_video.py
@@ -1,8 +1,8 @@
 import sys
 import subprocess
 import os
+import time
 from pathlib import Path
-from time import sleep
 from datetime import datetime
 
 '''
@@ -12,7 +12,7 @@ Linux: ffmpeg
 '''
 
 
-def time():
+def currenttime():
     now = datetime.now()
     return now.strftime("%d/%m/%Y:%H:%M:%S")
 
@@ -71,7 +71,7 @@ class compress_video():
                 self.video_path = self.lines[0]
             except IndexError:
                 print('{0} >> {1} >> No more videos to compress'.format(
-                    self.file_name, time()))
+                    self.file_name, currenttime()))
                 break
             self.video_path = self.video_path.replace('\n', '')
             self.nvideo_path = self.video_path.replace(
@@ -79,31 +79,34 @@ class compress_video():
             self.video_path = str(Path(self.video_path))
             self.nvideo_path = str(Path(self.nvideo_path))
             print('{0} >> {1} >> progress: {2}/{3} {4}%'.format(
-                self.file_name, time(), self.progress, self.total, format(self.progress/self.total*100, '.2f')))
+                self.file_name, currenttime(), self.progress, self.total, format(self.progress/self.total*100, '.2f')))
             print('{0} >> {1} >> video_path: {2}'.format(
-                self.file_name, time(), self.video_path))
+                self.file_name, currenttime(), self.video_path))
             print('{0} >> {1} >> temp_path: {2}'.format(
-                self.file_name, time(), self.nvideo_path))
+                self.file_name, currenttime(), self.nvideo_path))
 
             # check file exists
             exist = os.path.isfile(self.video_path)
             if not exist:
                 l = '{0} >> {1} >> {2} doesn\'t exist'.format(
-                    self.file_name, time(), self.video_path)
+                    self.file_name, currenttime(), self.video_path)
                 print(l)
                 with open(self.record_path, 'a') as f:
                     f.write(l + '\n')
                 self.lines.pop(0)
                 continue
             print('{0} >> {1} >> processing {2}'.format(
-                self.file_name, time(), os.path.basename(self.video_path)))
+                self.file_name, currenttime(), os.path.basename(self.video_path)))
 
             # get video duration
             ffprobe_cmd = 'ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "{0}"'.format(
                 self.video_path)
-            duration = subprocess.check_output(ffprobe_cmd, shell=self.shell)
+            duration = ((subprocess.check_output(
+                ffprobe_cmd, shell=self.shell)).decode('utf-8')).replace('\r\n', '')
+            duration = time.strftime('%H:%M:%S', time.gmtime(float(duration)))
+            # turn second into hours
             print('{0} >> {1} >> video duration: {2}'.format(
-                self.file_name, time(), duration.decode('utf-8')))
+                self.file_name, currenttime(), duration))
 
             # get video size pre-compression
             ffprobe_cmd = 'ffprobe -v error -show_entries format=size -of default=nokey=1:noprint_wrappers=1 "{0}"'.format(
@@ -121,9 +124,9 @@ class compress_video():
                 cmd_line = p.stdout.readline()
                 if not cmd_line:
                     break
-                print(cmd_line.decode('utf-8'), end='\r')
+                print(cmd_line.decode('utf-8'))
             print('{0} >> {1} >> compressed {2}'.format(
-                self.file_name, time(), os.path.basename(self.nvideo_path)))
+                self.file_name, currenttime(), os.path.basename(self.nvideo_path)))
 
             # recycle the old video
             if self.shell:
@@ -139,15 +142,15 @@ class compress_video():
             subprocess.call(recycle_cmd, shell=self.shell)
             if self.del_old:
                 print('{0} >> {1} >> deleted {2}'.format(
-                    self.file_name, time(), os.path.basename(self.video_path)))
+                    self.file_name, currenttime(), os.path.basename(self.video_path)))
             else:
                 print('{0} >> {1} >> recycled {2}'.format(
-                    self.file_name, time(), os.path.basename(self.video_path)))
+                    self.file_name, currenttime(), os.path.basename(self.video_path)))
 
             # rename the new video
             os.rename(self.nvideo_path, self.video_path)
             print('{0} >> {1} >> renamed {2}'.format(
-                self.file_name, time(), os.path.basename(self.nvideo_path)))
+                self.file_name, currenttime(), os.path.basename(self.nvideo_path)))
 
             # get video size post-compression
             ffprobe_cmd = 'ffprobe -v error -show_entries format=size -of default=nokey=1:noprint_wrappers=1 "{0}"'.format(
@@ -157,7 +160,7 @@ class compress_video():
 
             # calculate compression ratio / size reduction
             l = '{0} >> {1} >> ratio: {2}% >> saved {3}MB >> compressed {4}'.format(
-                self.file_name, time(), format(self.postpsize/self.prepsize*100, '.4f'), format((self.prepsize-self.postpsize)/(2**23), '10.2f'), os.path.basename(self.video_path))
+                self.file_name, currenttime(), format(self.postpsize/self.prepsize*100, '.4f'), format((self.prepsize-self.postpsize)/(2**23), '10.2f'), os.path.basename(self.video_path))
             print(l)
             with open(self.record_path, 'a') as f:
                 f.write(l + '\n')
@@ -167,7 +170,7 @@ class compress_video():
                 self.lines.pop(0)
             except Exception as e:
                 print('{0} >> {1} >> exception: {2}'.format(
-                    self.file_name, time(), e))
+                    self.file_name, currenttime(), e))
             if self.log_remove:
                 with open(self.file_path, 'w') as f:
                     f.writelines(self.lines)
@@ -177,7 +180,7 @@ class compress_video():
 
         # end
         print('\n{0} >> {1} >> finished compressing all designated files.'.format(
-            self.file_name, time()))
+            self.file_name, currenttime()))
 
 
 # main
