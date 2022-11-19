9266b81b0f41610e97c7255e2c33c73002de629f
belongtothenight <dachuan516@gmail.com>
2022-11-20 01:56:57 +0800

    list_file.py v1.0.0

---
+++ src/list_file.py
@@ -0,0 +1,46 @@
+import os
+import pathlib
+
+
+class export_file_log():
+    def __init__(self):
+        os.system('cls')
+        while True:
+            dir = input('Enter the path to the root of exporting files: ')
+            self.dir = pathlib.Path(dir)
+            path = input('Enter the path to export file log: ')
+            self.path = pathlib.Path(path)
+            format = input('Enter the format of files to export: ')
+            self.format = format
+            print()
+            print('Root of exporting files: ' + str(self.dir))
+            print('Export file log path: ' + str(self.path))
+            print('Format of files to export: ' + self.format)
+            print()
+            if input('Is this correct? (y/n): ') == 'y':
+                break
+
+    def export(self):
+        print('Exporting...')
+        for file in self.dir.rglob('*'):
+            file = pathlib.Path(file)
+            if file.is_file():
+                ext = (os.path.splitext(
+                    os.path.basename(file))[1]).lstrip('.')
+                if ext == self.format:
+                    print(file, end='\r')
+                    with open(self.path, 'a', encoding='utf-8') as f:
+                        f.write(str(file) + '\n')
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            efl = export_file_log()
+            efl.export()
+        except Exception as e:
+            print(e)
+        print()
+        con = input('Continue? (y/n): ')
+        if con == 'n':
+            break
