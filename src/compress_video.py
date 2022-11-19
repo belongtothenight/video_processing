3cbcd47c8396ace14fdb194b0c473032902eae86
belongtothenight <dachuan516@gmail.com>
2022-11-20 03:20:02 +0800

    compress_video.py v1.0.15
    fix txt encoding bug

--- src/compress_video.py
+++ src/compress_video.py
@@ -57,7 +57,7 @@ class compress_video():
 
     def read_log(self):
         # get the log file contents
-        with open(self.file_path, 'r') as f:
+        with open(self.file_path, 'r', encoding='utf-8') as f:
             self.lines = f.readlines()
 
     def remove_identical_lines(self):
@@ -93,7 +93,7 @@ class compress_video():
                 l = '{0} >> {1} >> {2} doesn\'t exist'.format(
                     self.file_name, currenttime(), self.video_path)
                 print(l)
-                with open(self.record_path, 'a') as f:
+                with open(self.record_path, 'a', encoding='utf-8') as f:
                     f.write(l + '\n')
                 self.lines.pop(0)
                 continue
@@ -166,7 +166,7 @@ class compress_video():
             l = '{0} >> {1} >> ratio: {2}% >> saved {3}MB >> compressed {4}'.format(
                 self.file_name, currenttime(), format(self.postpsize/self.prepsize*100, '3.4f'), format((self.prepsize-self.postpsize)/(2**23), '10.2f'), os.path.basename(self.video_path))
             print(l)
-            with open(self.record_path, 'a') as f:
+            with open(self.record_path, 'a', encoding='utf-8') as f:
                 f.write(l + '\n')
 
             # remove the line from the log file
@@ -176,7 +176,7 @@ class compress_video():
                 print('{0} >> {1} >> exception: {2}'.format(
                     self.file_name, currenttime(), e))
             if self.log_remove:
-                with open(self.file_path, 'w') as f:
+                with open(self.file_path, 'w', encoding='utf-8') as f:
                     f.writelines(self.lines)
             if self.progress >= self.total:
                 break
